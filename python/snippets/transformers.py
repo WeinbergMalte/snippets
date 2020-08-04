b11e70535320cafa20e2ea35c9db0fb1636a07bf
Malte Weinberg <malte.weinberg@ottogroup.com>
2020-08-04 09:35:19 +0200

    Add dict-option to CategoryTargetEncoder

--- python/snippets/transformers.py
+++ python/snippets/transformers.py
@@ -16,16 +16,26 @@ class CategoryTargetEncoder(TransformerMixin):
 
     Parameters
     ----------
-    cols: str, list, tuple
-        Column name or list of column names that should be encoded.
+    cols: str or list of str or tuple of str or dict of str:str
+        Column name, list of column names or dict of column names
+        that should be encoded. If type is dict, encoded columns
+        are mapped from key->value name and the original column is kept.
     target_col: str
         Name of target column.
-    ordinal_transform: bool
+    ordinal_transform: bool, optional default=True
         Specifies if transformed columns should be returned as ordinals.
     """
 
     def __init__(self, cols, target_col, ordinal_transform=False):
-        self.cols = listify(cols)
+
+        if isinstance(cols, dict):
+            aliases = list(cols.values())
+            cols = list(cols.keys())
+        else:
+            aliases = None
+
+        self.cols = to_element(cols)
+        self.aliases = aliases
         self.target_col = target_col
         self.ordinal_transform = ordinal_transform
 
@@ -34,10 +44,12 @@ class CategoryTargetEncoder(TransformerMixin):
 
     def transform(self, X):
 
+        if self.aliases:
+            X[self.aliases] = X[self.cols]
+            self.cols = self.aliases
+
         t_enc = TargetEncoder(cols=self.cols)
-
         X = t_enc.fit_transform(X, X[self.target_col])
-
         if not self.ordinal_transform:
             return X
 

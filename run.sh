a23cef1738654af558abc383236138dc5acb13cc
WeinbergMalte <malte.weinberg@ottogroup.com>
2021-02-14 13:47:18 +0100

    Remove dotfiles

--- run.sh
+++ run.sh
@@ -1,47 +1,7 @@
 #!/usr/bin/env bash
-
 set -e
 
-
-#### Functions and tasks
-
-
-function make_symlink {
-  local source=$1
-  local destination=$2
-  if [ -h "$destination" ]; then
-    return 0
-  elif [ -f "$destination" ]; then
-    echo "File exists: ${destination}"
-  else
-    ln -sv "$(pwd)"/"$source" "$destination"
-  fi
-}
-
-function task_setup_shell {
-  make_symlink dotfiles/shell/bashrc ~/.bashrc
-  make_symlink dotfiles/shell/bash_aliases ~/.bash_aliases
-}
-
-function task_setup_vim {
-  # install neovim:
-  sudo apt-get install neovim
-
-  # vim and neovim dotfiles and color scheme
-  mkdir -p ~/.config/nvim/colors
-  cp dotfiles/vim/plugin/gruvbox.vim ~/.config/nvim/colors/
-  make_symlink dotfiles/vim/vimrc ~/.vimrc
-  make_symlink dotfiles/vim/vimrc ~/.config/nvim/init.vim
- }
-
-function task_setup_vscode {
-  sudo apt update
-  sudo apt install software-properties-common apt-transport-https wget
-  wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-  sudo apt update
-  sudo apt install code
-}
+#### functions and tasks ##############################################################
 
 function ensure_conda_env {
   local conda_env="snippets"
@@ -87,10 +47,9 @@ function unit_test_python {
   (cd python && pytest)
 }
 
-#### name tasks and task usage ####
+#### name tasks and task usage ########################################################
 task_usage() {
-  echo "Usage: ./run.sh lint | format | unit-test |
-                setup-shell | setup-vim | setup-code | setup-conda"
+  echo "Usage: ./run.sh lint | format | unit-test | setup-conda"
   exit 1
 }
 cmd=$1
@@ -99,9 +58,6 @@ case "$cmd" in
   lint) linting ;;
   format) format_python ;;
   unit-test) unit_test_python ;;
-  setup-shell) task_setup_shell ;;
-  setup-vim) task_setup_vim ;;
-  setup-code) task_setup_vscode ;;
   setup-conda) ensure_conda_env ;;
   *)     task_usage ;;
 esac

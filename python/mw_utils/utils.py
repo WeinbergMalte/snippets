2b1cbc8a6d1d6becc54300977c5ce2ca08c5fc84
WeinbergMalte <malte.weinberg@ottogroup.com>
2021-05-21 11:27:06 +0200

    Add pd analysis util funcs

--- python/mw_utils/utils.py
+++ python/mw_utils/utils.py
@@ -1,6 +1,7 @@
 """ Module containing utility functions. """
 
 import numpy as np
+import pandas as pd
 from typing import Union
 
 
@@ -9,3 +10,31 @@ def listify(x) -> Union[list, tuple, np.ndarray]:
     if not isinstance(x, (list, tuple, np.ndarray)):
         return [x]
     return x
+
+
+def pd_column_analysis(
+    ds: pd.Series, value_col: str = "values", sort: str = None
+) -> pd.DataFrame:
+    """Returns formatted dataframe from standard column analysis"""
+
+    df = ds.reset_index().rename(columns={"index": "columns", 0: value_col})
+
+    if sort is None:
+        return df
+
+    if sort.lower().startswith("asc"):
+        df = df.sort_values(by=value_col, ascending=True)
+
+    if sort.lower().startswith("desc"):
+        df = df.sort_values(by=value_col, ascending=False)
+
+    return df.reset_index(drop=True)
+
+
+def pd_null_ratio(df: pd.DataFrame, value_col: str = "null_ratio") -> pd.DataFrame:
+    """Retrurns styled dataframe with null ratios of columns."""
+
+    df_nulls = pd_column_analysis(df.isnull().mean(), value_col=value_col)
+    return df_nulls.style.format("{:.2%}", subset=[value_col]).background_gradient(
+        cmap="Reds", vmin=0.0, vmax=1.0
+    )
---
+++ python/tests/test_plotting.py
@@ -0,0 +1,5 @@
+"""Contains tests for transformers module."""
+# pylint: disable=missing-docstring
+
+def test_1():
+    pass
